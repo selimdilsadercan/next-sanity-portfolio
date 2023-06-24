291a9fdbd91774dc990823dfa6fb59632a1c8b00
selimdilsad <selimdilsadercan@gmail.com>
2023-06-24 22:03:02 +0300

    bir ufak daha hata daha

--- src/app/(site)/[slug]/page.tsx
+++ src/app/(site)/[slug]/page.tsx
@@ -10,13 +10,17 @@ export default async function Page({ params }: Props) {
 
   return (
     <div>
-      <h1 className="text-5xl font-extrabold text-transparent bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text drop-shadow">
-        {page.title}
-      </h1>
+      {page.title && page.content && (
+        <>
+          <h1 className="text-5xl font-extrabold text-transparent bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text drop-shadow">
+            {page.title}
+          </h1>
 
-      <div className="mt-10 text-lg text-gray-700">
-        <PortableText value={page.content} />
-      </div>
+          <div className="mt-10 text-lg text-gray-700">
+            <PortableText value={page.content} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
